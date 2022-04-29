1c291ddfd55430e9d9473ab1f624a9ae47ec11c2
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-04-29 12:52:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,10 +26,44 @@ const todo5 = {
 
 const todoList = [todo1, todo2, todo3, todo4, todo5];
 
-function logToDos (todoArray){
+// function logToDos (todoArray){
+//     for (const todo of todoArray) {
+//         console.log(todo.name + " - Priorità: " + todo.priority)
+//     }
+// }
+
+
+
+// /// getElementById  // PRENDE UN ELEMENTO ALL'INTERNO DEL DOCUMENTO HTML 
+// const container = document.getElementById("todo-container");
+
+// /// createElement   // CREA UN ELEMENTO ALL'INTERNO DEL DOCUMENTO HTML
+// const par = document.createElement("p");
+
+// /// createTextNode  // CREA DEL TESTO ALL'INTERNO DEL DOCUMENTO HTML
+// const textNode = document.createTextNode('Ciao');
+
+// /// appendChild     // AGGIUNGE UN FIGLIO ALL'INTERNO DI UNA VARIABILE ALL'INTERNO DI UN DOCUMENTO HTML
+// par.appendChild(textNode);
+// container.appendChild(par);
+
+
+function writeToDosToDocument(todoArray) {
+    const container = document.getElementById('todo-container');
+    const ul = document.createElement('ul')
     for (const todo of todoArray) {
-        console.log(todo.name + " - Priorità: " + todo.priority)
+        const li = document.createElement('li');
+        li.classList.add('list-element');
+        const node = document.createTextNode(todo.name);
+        const priorityNode = document.createTextNode(' - Priorità: ' + todo.priority)
+        const br = document.createElement('br');
+        li.appendChild(node);
+        li.appendChild(br)
+        li.appendChild(priorityNode);
+        ul.appendChild(li)
+        container.appendChild(ul);
+        
     }
 }
 
-logToDos(todoList)
+writeToDosToDocument(todoList);
